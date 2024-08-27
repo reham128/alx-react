3eae0922e38f33bb8628f7a5d942a34939a958ac
Reham <r_nour30@yahoo.com>
2024-08-27 04:08:51 +0300

    up

--- 0x02-react_props/task_2/dashboard/src/Notifications/NotificationItem.js
+++ 0x02-react_props/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -1,16 +1,11 @@
-import React from 'react';
-import './Notifications.css';
+import React from "react";
+import "./Notifications.css";
 
 function NotificationItem({ type, html, value }) {
   return (
     <>
       {type && value ? <li data-notification-type={type}>{value}</li> : null}
-      {html ? (
-        <li
-          data-notification-type={type}
-          dangerouslySetInnerHTML={{ __html: html }}
-        ></li>
-      ) : null}
+      {html ? <li data-urgent dangerouslySetInnerHTML={{ __html: html }}></li> : null}
     </>
   );
 }
