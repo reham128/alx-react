f0f1b1d6e5940b0342d5df484a7fae37b871fd7e
Reham <r_nour30@yahoo.com>
2024-08-29 20:03:16 +0300

    app.js

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Notifications from "../Notifications/Notifications";
 import Header from "../Header/Header";
@@ -8,31 +8,35 @@ import CourseList from '../CourseList/CourseList';
 import './App.css';
 import { getLatestNotification } from '../utils/utils';
 
-function App({ isLoggedIn }) {
-  const listCourses = [
-    { id: 1, name: 'ES6', credit: 60 },
-    { id: 2, name: 'Webpack', credit: 20 },
-    { id: 3, name: 'React', credit: 40 },
-  ];
+class App extends Component {
+  render() {
+    const { isLoggedIn } = this.props;
 
-  const listNotifications = [
-    { id: 1, type: 'default', value: 'New course available' },
-    { id: 2, type: 'urgent', value: 'New resume available' },
-    { id: 3, type: 'urgent', html: getLatestNotification() },
-  ];
+    const listCourses = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 },
+    ];
 
-  return (
-    <React.Fragment>
-      <Notifications listNotifications={listNotifications} />
-      <div className='App'>
-        <Header />
-        <div className='App-body'>
-          {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+    const listNotifications = [
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New resume available' },
+      { id: 3, type: 'urgent', html: getLatestNotification() },
+    ];
+
+    return (
+      <React.Fragment>
+        <Notifications listNotifications={listNotifications} />
+        <div className='App'>
+          <Header />
+          <div className='App-body'>
+            {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </React.Fragment>
-  );
+      </React.Fragment>
+    );
+  }
 }
 
 App.defaultProps = {
--- 0x03-React_component/task_0/dashboard/src/Notifications/NotificationItem.js
+++ 0x03-React_component/task_0/dashboard/src/Notifications/NotificationItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Notifications.css";
+import PropTypes from 'prop-types';
 
 function NotificationItem({ type, html, value }) {
   return (
