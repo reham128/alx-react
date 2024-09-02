90f0c56e5179702a3d76b8a6ec8b6e9bb6e09166
Reham <r_nour30@yahoo.com>
2024-09-02 20:23:25 +0300

    task 4

---
+++ 0x03-React_component/task_4/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import './Notifications.css';
+import closeIcon from '../assets/close-icon.png';
+import NotificationItem from './NotificationItem';
+import PropTypes from 'prop-types';
+import NotificationItemShape from './NotificationItemShape';
+
+
+class Notifications extends Component {
+  constructor(props) {
+    super(props);
+    this.markAsRead = this.markAsRead.bind(this);
+  }
+  markAsRead(id) {
+    console.log(`Notification ${id} has been marked as read`);
+  }
+  render() {
+    const { displayDrawer, listNotifications } = this.props;
+
+    return (
+      <div className='notification-wrapper'>
+        <div className='menuItem'>
+          <p>Your notifications</p>
+        </div>
+        {displayDrawer && (
+          <div className='Notifications'>
+            <ul>
+              {listNotifications?.length ? (
+                <>
+                  <p>Here is the list of notifications</p>
+                  {listNotifications.map(({ id, html, type, value }) => (
+                    <NotificationItem
+                      key={id}
+                      type={type}
+                      value={value}
+                      html={html}
+                      id={id}
+                      markAsRead={this.markAsRead}
+                    />
+                  ))}
+                </>
+              ) : (
+                <li data-notification-type='default'>
+                  No new notification for now
+                </li>
+              )}
+            </ul>
+            <button
+              style={{
+                background: 'none',
+                border: 'none',
+                position: 'absolute',
+                right: '.8rem',
+                top: '1rem',
+                cursor: 'pointer',
+              }}
+              aria-label='Close'
+              onClick={() => console.log('Close button has been clicked')}
+            >
+              <img src={closeIcon} alt='closeIcon' width='18px' />
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+Notifications.propTypes = {
+  displayDrawer: PropTypes.bool,
+  listNotifications: PropTypes.arrayOf(NotificationItemShape),
+};
+
+Notifications.defaultProps = {
+  displayDrawer: false,
+  listNotifications: [],
+};
+
+export default Notifications;
