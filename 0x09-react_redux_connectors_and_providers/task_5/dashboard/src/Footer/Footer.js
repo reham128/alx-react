14ece72bdf09b090f0997959e92b156954e5e1c9
Reham <r_nour30@yahoo.com>
2024-10-03 00:51:22 +0300

    task 5

---
+++ 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/Footer/Footer.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { getFullYear, getFooterCopy } from "../utils/utils";
+import "./Footer.css";
+import { AppContext } from '../App/AppContext';
+
+function Footer() {
+  return (
+    <AppContext.Consumer>
+      {(context) => {
+        return (
+          <div className='App-footer'>
+            Copyright {getFullYear()} - {getFooterCopy()}
+            {context.user.isLoggedIn && (
+              <p>
+                <a href='/contact' id='contactLink'>
+                  Contact us
+                </a>
+              </p>
+            )}
+          </div>
+        );
+      }}
+    </AppContext.Consumer>
+  );
+}
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.get('user'),
+  };
+};
+
+export default connect(mapStateToProps, null)(Footer);
